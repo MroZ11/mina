03ba417733f60562c6d3912e456fe746b38580d5
小刀雨斑 <34430446+MroZ11@users.noreply.github.com>
2018-06-28 12:51:52 +0800

    Update Test.java

--- 源码（一个服务端，一个客服端）/test/src/com/zs/Test.java
+++ 源码（一个服务端，一个客服端）/test/src/com/zs/Test.java
@@ -34,28 +34,6 @@ import java.util.concurrent.TimeUnit;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import org.apache.commons.codec.digest.Md5Crypt;
 import org.apache.mina.filter.codec.ProtocolCodecFilter;
 import org.apache.mina.filter.codec.textline.TextLineCodecFactory;
@@ -74,194 +52,21 @@ public class Test {
 	public static void main(String[] args) throws Exception {
 		
 		try {
-			//µÚÒ»²½ ´´½¨Ò»¸öNioSocketAcceptor ¶ÔÏó  
+			//ç¬¬ä¸€æ­¥ åˆ›å»ºä¸€ä¸ªNioSocketAcceptor å¯¹è±¡  
 			NioSocketAcceptor  acceptor  = new 	NioSocketAcceptor();
-			//µÚ¶ş²½ÉèÖÃhandler  
+			//ç¬¬äºŒæ­¥è®¾ç½®handler  
 			acceptor.setHandler(new MyServerHandler());
-			//µÚÈı²½,»ñÈ¡À¹½ØÆ÷£¬·¢À´µÄÏûÏ¢¶¼ĞèÒªÍ¨¹ıÀ¹½ØÆ÷À¹½ØÖ®ºó²ÅÄÜ½ÓÊÕµ½  
-			//Ìí¼ÓÒ»¸öÀ¹½ØÆ÷¶ÔÊı¾İ½øĞĞ¼Ó½âÂëTextLineCodecFactory()  
+			//ç¬¬ä¸‰æ­¥,è·å–æ‹¦æˆªå™¨ï¼Œå‘æ¥çš„æ¶ˆæ¯éƒ½éœ€è¦é€šè¿‡æ‹¦æˆªå™¨æ‹¦æˆªä¹‹åæ‰èƒ½æ¥æ”¶åˆ°  
+			//æ·»åŠ ä¸€ä¸ªæ‹¦æˆªå™¨å¯¹æ•°æ®è¿›è¡ŒåŠ è§£ç TextLineCodecFactory()  
 			acceptor.getFilterChain().addLast("CodeFilter", new ProtocolCodecFilter(new TextLineCodecFactory()));
-			//µÚËÄ²½£¬°ó¶¨¶Ë¿ÚºÅ  
+			//ç¬¬å››æ­¥ï¼Œç»‘å®šç«¯å£å·  
 			acceptor.bind(new InetSocketAddress("127.0.0.1", 9898));
 			
 
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
-		
-		
-		
-		
-		/*double[][] t  = {{1,2},
-								{3,4}};
-		
-		
-		double[][] t2 =  {{2,3,4},
-								{5,6,7}};
-		
-		Matrix m1 = new Matrix(t);
-		Matrix m2 = new Matrix(t2);
-		Matrix m3=  m1.times(m2);
-		m3.print(10, 0);*/
-
-		
-		/*RandomAccessFile infFile = new RandomAccessFile("E:/1.txt", "rw");
-		RandomAccessFile outfFile = new RandomAccessFile("E:/data/w.txt", "rw");
-		outfFile.seek(outfFile.length());
-		
-		ByteBuffer buffer = ByteBuffer.allocate(48);
-		FileChannel inChannel = infFile.getChannel();
-		FileChannel outChannel = outfFile.getChannel();
-		
-		int currentDateNum = inChannel.read(buffer);
-		
-		while (currentDateNum!=-1) {
-			buffer.flip();
-			while (buffer.hasRemaining()) {
-				outChannel.write(buffer);
-			}
-			buffer.clear();
-			currentDateNum = inChannel.read(buffer);
-		}
-		
-		inChannel.close();
-		outChannel.close();
-		infFile.close();
-		outfFile.close();*/
-		
-		/*String a= "2";
-		String b= "3";
-		String c = "13+0.111";
-		ScriptEngineManager manager = new ScriptEngineManager();
-		ScriptEngine engine = manager.getEngineByName("js");
-		Double hour = (Double)engine.eval("300/1700");*/
-	
-		
-		 /*Scanner in = new Scanner(System.in);
-		  float f = in.nextFloat();
-		  int fInt = (int) f;
-		  BigDecimal b1 = new BigDecimal(Float.toString(f));
-		  BigDecimal b2 = new BigDecimal(Integer.toString(fInt));
-		  float fPoint = b1.subtract(b2).floatValue();
-		  System.out.println("ÕûÊı²¿·ÖÎª:" + fInt + "\n" + "Ğ¡Êı²¿·ÖÎª: " + fPoint);*/
-		
-		/*BigDecimal b1 = new BigDecimal(2.2521);
-		b1.setScale(2,BigDecimal.ROUND_DOWN);
-		System.out.println(b1.setScale(2,BigDecimal.ROUND_DOWN).toString());*/
-		
-		/*Calendar calendar = Calendar.getInstance();
-		calendar.set(2017,10,17);
-		
-		Calendar calendar2 = Calendar.getInstance();
-		calendar2.set(2017,10,2);
-		
-		long a = calendar.getTimeInMillis();
-		long b= calendar2.getTimeInMillis();
-		System.out.println((b-a)/(1000*3600*24));*/
-		
-		/*writeOut();
-	    System.out.println((int)((Math.random()*9+1)*100000));  */
-		
-		
-		/*Timer timer = new Timer();
-		Calendar calendar = Calendar.getInstance();
-		calendar.set(Calendar.HOUR_OF_DAY, 13);
-		calendar.set(Calendar.MINUTE, 20);
-		calendar.set(Calendar.SECOND, 0); 
-		Date date = calendar.getTime();
-
-		timer.schedule(new TimerTask() {
-			@Override
-			public void run() {
-				System.out.println("ÕâÊÇÒ»¸ö¶¨Ê±ÈÎÎñ");
-				
-			}
-		}, date,2000);*/
-		
-		/*Runnable runnable = new Runnable() {
-			@Override
-			public void run() {
-				System.out.println("ÕâÊÇÒ»¸ö¶¨Ê±ÈÎÎñ");
-			}
-		};
-	
-		ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
-		executorService.scheduleAtFixedRate(runnable, 2,2, TimeUnit.SECONDS);*/
-		
-		
-		
-		/*TestRunable runable = new TestRunable();
-		TestThread testThread = new TestThread();
-		Thread thread = new Thread(runable,"Ïß³ÌÒ»Ò»Ò»Ò»Ò»Ò»£º");
-		testThread.setName("Ïß³ÌØ­Ø­Ø­Ø­Ø­Ø­Ø­Ø­£º");
-		MyCallable callable = new MyCallable();
-	
-		
-		ExecutorService fixedThreadPool  = 	 Executors.newFixedThreadPool(2);
-		fixedThreadPool.shutdown();
-		ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(5); 
-		
-		scheduledThreadPool.schedule(thread, 2, TimeUnit.SECONDS);
-		scheduledThreadPool.awaitTermination(1, TimeUnit.SECONDS);
-		scheduledThreadPool.schedule(testThread, 1, TimeUnit.SECONDS);
-		 
-		ScheduledFuture future = scheduledThreadPool.schedule(callable, 1, TimeUnit.SECONDS);
-
-		try {
-			System.out.println(future.get());
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ExecutionException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
-		
 	}
-
-	
-	
-	public static void writeOut(){
-		boolean run = true;
-		int succes=0;
-		int wrong=0;
-		
-		int count =0;
-		while (run) {
-			int a  =Math.round((float)Math. random()*100);
-			int b  =Math.round((float)Math. random()*100);
-			System.out.println(a+"+"+b+"=?");
-			String anws = null;
-			Scanner scanner = new Scanner(System.in);
-			anws = scanner.next();
-			int c = a+ b;
-			if(anws .equals("y")||anws.equals("Y")){
-				NumberFormat numberFormat = NumberFormat.getInstance();
-				numberFormat.setMaximumFractionDigits(2);
-				System.out.println("×Ü¹²:"+count+";ÕıÈ·£º"+succes+";´íÎó£º"+wrong+";ÕıÈ·ÂÊ£º"+numberFormat.format((float) succes / (float) count * 100));
-				run=false;
-				break;
-			}
-			try {
-				if(c==Integer.parseInt(anws)){
-					succes++;
-					count++;
-					System.out.println("¡Ì¡Ì¡Ì¡Ì¡Ì¡Ì¡ÌÕıÈ·¡Ì¡Ì(ÊäÈëY½áÊø)¡Ì¡Ì");
-				}else{
-					wrong++;
-					count++;
-					System.out.println("XXXXXXX´íÎóXX(ÊäÈëY½áÊø)XX");
-				}
-			} catch (Exception e) {
-				wrong++;
-				count++;
-				System.out.println("XXXXXXX´íÎóXX(ÊäÈëY½áÊø)XX");
-			}
-		}
-	
-	}
-	
-	
 	
 	
 }
